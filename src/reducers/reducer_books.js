dbe1a396210d80403f1d969d247daf5757e0262d
Yamit Villamil <yamit.villamil@globant.com>
2018-07-07 20:41:36 -0500

    Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The good parts'},
+        { title: 'Harry Potter'},
+        { title: 'The dark tower'},
+        { title: 'Eloquent Ruby'},
+    ];
+}
